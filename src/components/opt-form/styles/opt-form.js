2de25a423877dbc2a67f09895f26f066baaebb70
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2021-03-15 15:39:35 +0200

    Opt form start and structure

---
+++ src/components/opt-form/styles/opt-form.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+    display: flex;
+    border-bottom: 8px solid #222;
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Button = styled.button`
+
+`;
+
+export const Text = styled.p`
+    
+`;
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import faqsData from '../fixtures/faqs.json'
 import { Accordion } from '../components'
+import OptForm from '../components/opt-form';
+
 
 export function FaqsContainer() {
     return (
@@ -12,6 +14,11 @@ export function FaqsContainer() {
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
+            <OptForm>
+                <OptForm.Input placeholder="Email address" />
+                <OptForm.Button>Try it now!</OptForm.Button>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+            </OptForm>
         </Accordion>
     );
 }
