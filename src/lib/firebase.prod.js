7a49b55efa14b0f6b175f682c957508f6a7b80c6
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2021-03-16 17:19:52 +0200

    firebase data seeding

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -2,10 +2,14 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-//we need to seed database
-
-// we need a config here
-const config = { };
+const config = {
+    apiKey: "AIzaSyDPKcn5Mt0EXbYkgHI_fj9-GsOwYqkeJxU",
+    authDomain: "netflix-756df.firebaseapp.com",
+    projectId: "netflix-756df",
+    storageBucket: "netflix-756df.appspot.com",
+    messagingSenderId: "296303056201",
+    appId: "1:296303056201:web:a5d67a620e53f98c0b7698"
+ };
 
 const firebase = Firebase.initializeApp(config);
 
