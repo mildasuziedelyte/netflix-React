6c379000298c678b7854da1116cdae2fbda81d08
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2021-03-17 14:15:22 +0200

    sign in done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import * as ROUTES from './constants/routes'
-import { Home, Browse, Signin, Signup } from './pages'
+import * as ROUTES from './constants/routes';
+import { Home, Browse, Signin, Signup } from './pages';
 
 export default function App() {
   return (
