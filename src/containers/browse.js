30517949b84279c8887039a21574960419638063
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2021-03-25 14:21:40 +0200

    header setup in browse page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/firebase'
-import { Loading } from '../components'
+import { Loading, Header } from '../components'
 
 export function BrowseContainer( {slides}) {
     const [profile, setProfile] = useState({});
@@ -16,9 +16,10 @@ export function BrowseContainer( {slides}) {
     }, [profile.displayName]);
 
     return profile.displayName ? (
-        loading ? (
-            <Loading src={user.photoURL} /> 
-        ) : <Loading.ReleaseBody />
+        <>
+        { loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody /> }
+        <Header src="joker1"><p>Hello</p></Header>
+        </>
     ) : (
         <SelectProfileContainer user={user} setProfile={setProfile}/>
     )
