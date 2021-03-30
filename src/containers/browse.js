840fb7d2c6fc6f9603875850e9d429ae98c0dd0c
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2021-03-30 11:58:01 +0300

    card logic and structure

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -4,6 +4,7 @@ import { FirebaseContext } from '../context/firebase'
 import { Card, Loading, Header } from '../components'
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
+import { TextSmall } from '../components/form/styles/form';
 
 export function BrowseContainer( {slides}) {
     const [searchTerm, setSearchTerm] = useState('');
@@ -76,6 +77,23 @@ export function BrowseContainer( {slides}) {
             {slideRows.map((slideItem) => (
                 <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
                     <Card.Title>{slideItem.title}</Card.Title>
+                    <Card.Entities>
+                        {slideItem.data.map((item) => (
+                            <Card.Item key={item.docId} item={item}>
+                                <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}/>
+                                <Card.Meta>
+                                    <Card.Subtitle>{item.title}</Card.Subtitle>
+                                    <Card.Text>{item.description}</Card.Text>
+                                </Card.Meta>
+                            </Card.Item>
+                        ))}
+                    </Card.Entities>
+                    {/* <Card.Feature category={category}>
+                        <Player>
+                            <Player.Button />
+                            <Player.Video src="/videos/bunny.mp4" />
+                        </Player>
+                    </Card.Feature> */}
                 </Card>
             ))}
         </Card.Group>
